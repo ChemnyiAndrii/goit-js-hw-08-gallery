dfc7996bf089da684b54cf2d6f5eadd268d72e49
Andrii <65915107+ChemnyiAndrii@users.noreply.github.com>
2020-11-01 22:11:19 +0200

    вношу виправлення
    працює кнопка закриття, та відслідковується бекдроп

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,7 +1,7 @@
-import picture from "./gallery-items.js";
+import picture from './gallery-items.js';
 
-const container = document.querySelector(".js-gallery");
-container.insertAdjacentHTML("beforeend", createGalleryItem(picture));
+const container = document.querySelector('.js-gallery');
+container.insertAdjacentHTML('beforeend', createGalleryItem(picture));
 
 function createGalleryItem(picture) {
   return picture
@@ -22,47 +22,49 @@ function createGalleryItem(picture) {
 </li>
     `;
     })
-    .join("");
+    .join('');
 }
 
-container.addEventListener("click", onContainerClick);
+container.addEventListener('click', onContainerClick);
 
-const lightBox = document.querySelector(".lightbox");
-lightBox.addEventListener("click", openModal);
+const lightBox = document.querySelector('.lightbox');
 
-const lightboxImage = document.querySelector(".lightbox__image");
+const lightboxImage = document.querySelector('.lightbox__image');
 
 const closeBth = document.querySelector('[data-action="close-lightbox"]');
-closeBth.addEventListener("click", closeModal);
+closeBth.addEventListener('click', closeModal);
 
 function openModal() {
-  window.addEventListener("keydown", escPress);
-  lightBox.classList.add("is-open");
+  window.addEventListener('keydown', escPress);
+  lightBox.classList.add('is-open');
 }
 function closeModal() {
-  window.removeEventListener("keydown", escPress);
-  lightBox.classList.remove("is-open");
+  window.removeEventListener('keydown', escPress);
+  lightBox.classList.remove('is-open');
 }
 function onContainerClick(event) {
   event.preventDefault();
-  if (event.target.nodeName !== "IMG") {
+  if (event.target.nodeName !== 'IMG') {
     return;
   }
   console.log(event.target.alt);
-  lightboxImage.src = "";
+  lightboxImage.src = '';
   openModal();
   lightboxImage.src = event.target.dataset.source;
   lightboxImage.alt = event.target.alt;
 }
 function escPress(event) {
-  const keyCode = "Escape";
+  const keyCode = 'Escape';
   const isEsc = event.code === keyCode;
   if (isEsc) {
     closeModal();
   }
 }
+const overLay = document.querySelector('.lightbox__overlay');
+overLay.addEventListener('click', backDrop);
 function backDrop(event) {
   if (event.currentTarget == event.target) {
-    closeModal();
+    
+    console.log("Клік в пустоту");
   }
 }
